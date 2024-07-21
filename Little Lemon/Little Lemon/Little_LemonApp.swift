d2eccf8bf95485cc562930646336b356f0004168
Joel Ruetas <jruetas@abellgroup.com>
2024-07-20 23:20:29 -0400

    Initial setup for Little Lemon app

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Joel Ruetas on 2024-07-20.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
